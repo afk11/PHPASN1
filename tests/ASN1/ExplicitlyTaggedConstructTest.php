afd7940af901628e6974b8e90a7012221afbfa49
Friedrich Große <friedrich.grosse@gmail.com>
2015-05-08 20:01:52 +0200

    Add tests to check where issue #36 is coming from

--- tests/ASN1/ExplicitlyTaggedConstructTest.php
+++ tests/ASN1/ExplicitlyTaggedConstructTest.php
@@ -89,11 +89,59 @@ class ExplicitlyTaggedObjectTest extends ASN1TestCase
     }
 
     /**
+     * This does not work since PHP shifts in 1 bits from the left
+     */
+    public function testPhpShiftOperatorWithLeadingOneBit()
+    {
+        $value = 3735928559;
+        // binary   = 11011110 10101101 10111110 11101111
+        // expected = 01101111 01010110 11011111 01110111
+        // actual   = 11101111 01010110 11011111 01110111
+        //$this->assertEquals(decbin(1867964279), decbin($value >> 1));
+
+        $this->assertEquals(1867964279, $value >> 1);
+        $this->assertEquals( 933982139, $value >> 2);
+        $this->assertEquals( 466991069, $value >> 3);
+        $this->assertEquals( 233495534, $value >> 4);
+        $this->assertEquals( 116747767, $value >> 5);
+        $this->assertEquals(  58373883, $value >> 6);
+        $this->assertEquals(  29186941, $value >> 7);
+    }
+
+    /**
+     * This does also work
+     */
+    public function testPhpShiftByDivisionWithLeadingOneBit()
+    {
+        $value = 3735928559;
+        $this->assertEquals(1867964279, intval($value / 2));
+        $this->assertEquals( 933982139, intval($value / 4));
+        $this->assertEquals( 466991069, intval($value / 8));
+        $this->assertEquals( 233495534, intval($value / 16));
+        $this->assertEquals( 116747767, intval($value / 32));
+        $this->assertEquals(  58373883, intval($value / 64));
+        $this->assertEquals(  29186941, intval($value / 128));
+    }
+
+    /**
+     * The following works
+     */
+    public function testPhpShiftOperatorWithLeadingZeroBit()
+    {
+        $value = 1588444911;
+        // binary   = 01011110 10101101 10111110 11101111
+        // expected = 00101111 01010110 11011111 01110111
+        $this->assertEquals(decbin(794222455), decbin($value >> 1));
+
+        $this->assertEquals(794222455, $value >> 1);
+    }
+
+    /**
      * @depends testGetBinary
+     * @depends testPhpShiftOperatorWithLeadingOneBit
      */
     public function testFromBinaryWithHugeTagNumber()
     {
-        $this->markTestSkipped('Skipped to see if this fixes the other tests on travis');
         $originalStringObject = new PrintableString('test');
         $originalObject = new ExplicitlyTaggedObject(0xDEADBEEF, $originalStringObject);
         $binaryData = $originalObject->getBinary();
